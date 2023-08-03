c244900669e9b4c7b6e706181c7b49a4d00ee6c5
Simon Maracine <simonmaracine@gmail.com>
2023-08-03 12:09:02 +0300

    Add setup scripts

---
+++ scripts/setup.bat
@@ -0,0 +1,7 @@
+echo off
+
+cd ..
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DMUHLE_SHARED_LIBRARY=ON -A x64
+cd ..\scripts
---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd ..
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DMUHLE_SHARED_LIBRARY=ON
+cd ../scripts
