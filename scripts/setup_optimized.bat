b0cd7115cfcc3d8b7f7326c6da89086845aec43a
SimonMaracine <simonmaracine@gmail.com>
2023-09-14 22:30:18 +0300

    Add Google Test as a submodule

--- scripts/setup_optimized.bat
+++ scripts/setup_optimized.bat
@@ -3,7 +3,7 @@ echo off
 cd ..
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DMUHLE_SHARED_LIBRARY=ON -A x64
+cmake .. -DCMAKE_BUILD_TYPE=Release -DMUHLE_SHARED_LIBRARY=ON -DMUHLE_BUILD_GUI=ON -DMUHLE_BUILD_TESTS=ON -A x64
 cd ..\scripts
 
 pause
